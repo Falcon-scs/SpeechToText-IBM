02bb35743028c2f9602d029b7bbae1ca1e7c7cb7
dev.maple <31434845+dev-maple@users.noreply.github.com>
2018-11-02 17:35:03 +0800

    Update token.php

--- token.php
+++ token.php
@@ -4,8 +4,8 @@
     error_reporting(E_ALL);
     ini_set('max_execution_time', 18000000000);
     $config = new Config();
-    $config->setUsername('d1dfee2b-02f1-466d-9476-a09705e6518b');
-    $config->setPassword('rr0LewXCMicq');
+    $config->setUsername('');
+    $config->setPassword('');
     $speechToText = new SpeechToText($config);
     $params = ['headers' => 'X-Brideo'];
     $speechToText->config->setConfig($params);
